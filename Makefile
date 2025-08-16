4eee9250aca439e23aa5df83f66cfa524832d1e2
Anders Thøgersen <anderslt@gmail.com>
2025-08-16 20:29:57 +0200

    danish

--- Makefile
+++ Makefile
@@ -15,12 +15,6 @@ endif
 build:
 	qmk compile -e CONVERT_TO=liatris -kb splitkb/aurora/sofle_v2 -km vial
 
-sofle-build.json: sofle-layout.json
-	sed -e 's/KC_F13/UP(DK_AE, DK_AE_UPPER)/g' \
-        -e 's/KC_F14/UP(DK_OE, DK_OE_UPPER)/g' \
-        -e 's/KC_F15/UP(DK_AA, DK_AA_UPPER)/g' \
-		-e 's/KC_F16/UC_PREV/g' \
-		-e 's/KC_F17/UC_NEXT/g' sofle-layout.json > sofle-build.json
-	qmk json2c sofle-build.json -o keyboards/splitkb/aurora/sofle_v2/keymaps/vial/sofle-layout.h
-	rm -fv sofle-build.json
+hfile: sofle-layout.json
 	+$(MAKE) -C $(QMK_FIRMWARE_ROOT) $(MAKECMDGOALS) QMK_USERSPACE=$(QMK_USERSPACE)
+
--- watch.sh
+++ watch.sh
@@ -11,6 +11,12 @@ while true; do
     case "$NEWFILE" in
         *splitkb_aurora_sofle_v2_rev1_layout_*)
             mv -v "$NEWFILE" sofle-layout.json
+            sed -e 's/KC_F13/UP(DK_AE, DK_AE_UPPER)/g' \
+                -e 's/KC_F14/UP(DK_OE, DK_OE_UPPER)/g' \
+                -e 's/KC_F15/UP(DK_AA, DK_AA_UPPER)/g' \
+	        	-e 's/KC_F16/UC_PREV/g' \
+	        	-e 's/KC_F17/UC_NEXT/g' sofle-layout.json > ./keyboards/splitkb/aurora/sofle_v2/keymaps/vial/sofle-build.json
+	        qmk json2c ./keyboards/splitkb/aurora/sofle_v2/keymaps/vial/sofle-build.json -o keyboards/splitkb/aurora/sofle_v2/keymaps/vial/sofle-layout.h
             make hfile
             ;;
         *)
