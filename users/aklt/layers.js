0d1501b865e76ae6d2ac4a0bc06f3841c2890b1b
Anders Thøgersen <anderslt@gmail.com>
2026-02-13 01:52:20 +0100

    WIP define keymap

---
+++ users/aklt/layers.js
@@ -0,0 +1,148 @@
+// Define layers
+// First column is the template key name, second is the key
+// Following columns are overrides for a specific keyboard
+//
+//
+// keyname  key  Ferris  Kyria  Sofle
+const layers = {
+    base: `
+   s00  GRV
+   s01  1
+   s02  2
+   s03  3
+   s04  4
+   s05  5
+   s06  6
+   s07  7
+   s08  8
+   s09  9
+   s10  0
+   s11  BSP
+
+   e12  TAB
+   f13  Q
+   f14  W
+   f15  E
+   f16  R
+   f17  T
+   f18  Y
+   f19  U
+   f20  I
+   f21  O
+   f22  P
+   e23  BSLS  k:BSP
+
+   e24  LSF
+   f25  A
+   f26  S
+   f27  D
+   f28  F
+   f29  G
+   f30  H
+   f31  J
+   f32  K
+   f33  L
+   f34  SCLN
+   e35  SENT
+
+   e36  LCTL
+   f37  Z
+   f38  X
+   f39  C
+   f40  V
+   f41  B
+   e42  SPC
+   k43  RGUI
+   k44  RALT
+   e45  RCTL
+   f46  N
+   f47  M
+   f48  COMM
+   f49  DOT
+   f50  SLSH
+   e51  RCTL
+
+   s52  LGUI
+   e53  L3_D
+   e54  L2_ESC
+   f55  LALT
+   f56  L1_LEAD
+   k57  WIR
+   k58  OPR
+   f57  L4_S
+   f58  GUI_DEL
+   e59  L5_F
+   e60  APP
+   s61  RGUI
+`,
+    qwerty: `
+   f13  q
+   f14  w
+   f15  e
+   f16  r
+   f17  t
+   f18  y
+   f19  u
+   f20  i
+   f21  o
+   f22  p
+
+   f25  a
+   f26  s
+   f27  d
+   f28  f
+   f29  g
+   f30  h
+   f31  j
+   f32  k
+   f33  k
+   f34  l
+
+   f37  z
+   f38  x
+   f39  c
+   f40  v
+   f41  b
+   f46  n
+   f47  m
+   f48  ,
+   f49  .
+   f50  /
+`,
+    colemak: `
+   f13  q
+   f14  w
+   f15  f
+   f16  p
+   f17  g
+   f18  j
+   f19  l
+   f20  u
+   f21  y
+   f22  ;
+
+   f25  a
+   f26  r
+   f27  s
+   f28  t
+   f29  d
+   f30  h
+   f31  n
+   f32  e
+   f33  i
+   f34  o
+
+   f37  z
+   f38  x
+   f39  c
+   f40  v
+   f41  b
+   f46  k
+   f47  m
+   f48  ,
+   f49  .
+   f50  /
+`,
+l1nav: `
+`
+};
