2d1f91cf4543bbf7021b3ba4c6b62debf4a7d45d
Anders Thøgersen <anderslt@gmail.com>
2026-02-18 22:16:16 +0100

    comment out

--- users/aklt/define-keymap.js
+++ users/aklt/define-keymap.js
@@ -267,100 +267,100 @@ combo_t key_combos[] = {
 }
 
 
-const colemakDHKeymap = `
-q w f p g           j l u y ;
-a r s t d           h n e i o
-z x c v b           k m , . /
-`;
-
-const numbers = "1 2 3 4 5 6 7 8 9 0";
-const symbols = `- = [ ] \\ ; ' , . / \``;
-
-const keysTemplate = `
-esc  1   2    3    4   5             6   7   8    9    0    bsp
-tab  q   w    e    r   t             y   u   i    o    p    \\
-shl  a   s    d    f   g             h   j   k    l    ;    entr
-ctl  z   x    c    v   b             n   m   ,    .    /    ctr
-         opl  wil  l1  l2  spc rgui  r2  r1  wir  opr
-`;
-
-const horizontalCombosTemplate = `
-k00-k01 k01-k02 k02-k03 k03-k04         k05-k06 k06-k07 k07-k08 k08-k09
-k10-k11 k11-k12 k12-k13 k13-k14         k15-k16 k16-k17 k17-k18 k18-k19
-k20-k21 k21-k22 k22-k23 k23-k24         k25-k26 k26-k27 k27-k28 k28-k29
-                        k30-k31 k31-k32 k32-k33
-`;
-
-const combos = {
-    "k00-k01": "KC_ESC",
-};
-
-// console.log(templateReplaceKeys(keymapTemplate))
-
-// console.log(keymapTemplate);
-
-const fullKeyboard = `
-esc   f1   f2   f3   f4     f5   f6   f7   f8     f9   f10  f11  f12          pscr slck pause
-
-\`    1    2    3    4    5    6    7    8    9    0    -    =    bspc        ins  home pgup      nlck  k/   k*  k-
-tab   q    w    e    r    t    y    u    i    o    p    [    ]    \\          del  end  pgdn      k7    k8   k9  k+
-caps  a    s    d    f    g    h    j    k    l    ;    '    entr                                 k4    k5   k6
-lsft  z    x    c    v    b    n    m    ,    .    /    rsft                        up            k1    k2   k3  kentr
-lctl  lgui lalt           spc                 ralt rgui menu rctl             left down rght      k0         k.
-`;
-
-// Additional key mappings for fullKeyboard that aren't basic alphanumeric/punctuation
-const othersToQmkCode = {
-    // Function keys
-    f1: "KC_F1",
-    f2: "KC_F2",
-    f3: "KC_F3",
-    f4: "KC_F4",
-    f5: "KC_F5",
-    f6: "KC_F6",
-    f7: "KC_F7",
-    f8: "KC_F8",
-    f9: "KC_F9",
-    f10: "KC_F10",
-    f11: "KC_F11",
-    f12: "KC_F12",
-    // Modifiers
-    lctl: "KC_LCTL",
-    lsft: "KC_LSFT",
-    lalt: "KC_LALT",
-    lgui: "KC_LGUI",
-    rctl: "KC_RCTL",
-    rsft: "KC_RSFT",
-    ralt: "KC_RALT",
-    rgui: "KC_RGUI",
-    // Navigation and editing
-    esc: "KC_ESC",
-    tab: "KC_TAB",
-    caps: "KC_CAPS",
-    entr: "KC_ENT",
-    bspc: "KC_BSPC",
-    spc: "KC_SPC",
-    menu: "KC_APP",
-    ins: "KC_INS",
-    del: "KC_DEL",
-    home: "KC_HOME",
-    end: "KC_END",
-    pgup: "KC_PGUP",
-    pgdn: "KC_PGDN",
-    // Arrow keys
-    up: "KC_UP",
-    down: "KC_DOWN",
-    left: "KC_LEFT",
-    rght: "KC_RGHT",
-    // Special function keys
-    pscr: "KC_PSCR",
-    slck: "KC_SCRL",
-    pause: "KC_PAUS",
-    nlck: "KC_NUM",
-    // Numpad operators
-    "*": "KC_PAST",
-    "+": "KC_PPLS",
-};
+// const colemakDHKeymap = `
+// q w f p g           j l u y ;
+// a r s t d           h n e i o
+// z x c v b           k m , . /
+// `;
+//
+// const numbers = "1 2 3 4 5 6 7 8 9 0";
+// const symbols = `- = [ ] \\ ; ' , . / \``;
+//
+// const keysTemplate = `
+// esc  1   2    3    4   5             6   7   8    9    0    bsp
+// tab  q   w    e    r   t             y   u   i    o    p    \\
+// shl  a   s    d    f   g             h   j   k    l    ;    entr
+// ctl  z   x    c    v   b             n   m   ,    .    /    ctr
+//          opl  wil  l1  l2  spc rgui  r2  r1  wir  opr
+// `;
+//
+// const horizontalCombosTemplate = `
+// k00-k01 k01-k02 k02-k03 k03-k04         k05-k06 k06-k07 k07-k08 k08-k09
+// k10-k11 k11-k12 k12-k13 k13-k14         k15-k16 k16-k17 k17-k18 k18-k19
+// k20-k21 k21-k22 k22-k23 k23-k24         k25-k26 k26-k27 k27-k28 k28-k29
+//                         k30-k31 k31-k32 k32-k33
+// `;
+//
+// const combos = {
+//     "k00-k01": "KC_ESC",
+// };
+//
+// // console.log(templateReplaceKeys(keymapTemplate))
+//
+// // console.log(keymapTemplate);
+//
+// const fullKeyboard = `
+// esc   f1   f2   f3   f4     f5   f6   f7   f8     f9   f10  f11  f12          pscr slck pause
+//
+// \`    1    2    3    4    5    6    7    8    9    0    -    =    bspc        ins  home pgup      nlck  k/   k*  k-
+// tab   q    w    e    r    t    y    u    i    o    p    [    ]    \\          del  end  pgdn      k7    k8   k9  k+
+// caps  a    s    d    f    g    h    j    k    l    ;    '    entr                                 k4    k5   k6
+// lsft  z    x    c    v    b    n    m    ,    .    /    rsft                        up            k1    k2   k3  kentr
+// lctl  lgui lalt           spc                 ralt rgui menu rctl             left down rght      k0         k.
+// `;
+//
+// // Additional key mappings for fullKeyboard that aren't basic alphanumeric/punctuation
+// const othersToQmkCode = {
+//     // Function keys
+//     f1: "KC_F1",
+//     f2: "KC_F2",
+//     f3: "KC_F3",
+//     f4: "KC_F4",
+//     f5: "KC_F5",
+//     f6: "KC_F6",
+//     f7: "KC_F7",
+//     f8: "KC_F8",
+//     f9: "KC_F9",
+//     f10: "KC_F10",
+//     f11: "KC_F11",
+//     f12: "KC_F12",
+//     // Modifiers
+//     lctl: "KC_LCTL",
+//     lsft: "KC_LSFT",
+//     lalt: "KC_LALT",
+//     lgui: "KC_LGUI",
+//     rctl: "KC_RCTL",
+//     rsft: "KC_RSFT",
+//     ralt: "KC_RALT",
+//     rgui: "KC_RGUI",
+//     // Navigation and editing
+//     esc: "KC_ESC",
+//     tab: "KC_TAB",
+//     caps: "KC_CAPS",
+//     entr: "KC_ENT",
+//     bspc: "KC_BSPC",
+//     spc: "KC_SPC",
+//     menu: "KC_APP",
+//     ins: "KC_INS",
+//     del: "KC_DEL",
+//     home: "KC_HOME",
+//     end: "KC_END",
+//     pgup: "KC_PGUP",
+//     pgdn: "KC_PGDN",
+//     // Arrow keys
+//     up: "KC_UP",
+//     down: "KC_DOWN",
+//     left: "KC_LEFT",
+//     rght: "KC_RGHT",
+//     // Special function keys
+//     pscr: "KC_PSCR",
+//     slck: "KC_SCRL",
+//     pause: "KC_PAUS",
+//     nlck: "KC_NUM",
+//     // Numpad operators
+//     "*": "KC_PAST",
+//     "+": "KC_PPLS",
+// };
 
 // Map key names to QMK keycodes
 function keyToQmkCodeOrDefine(key, unknownKey = "_______") {
